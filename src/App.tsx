8f71204e086d2b480482f1de15b021c42a65ee5a
kousuke_ida <takashiii1192@gmail.com>
2020-11-28 18:32:54 +0900

    add: eslint, prettier, webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { MenuBar } from "./components/MenuBar";
-import "./app.css"
+import "./app.css";
 
 export const App: React.FC<{}> = () => {
   return (
-  <div>
-    <MenuBar></MenuBar>
-    <div className="Content">
-      hello, world<br></br>
-      このページは現在工事中です
+    <div>
+      <MenuBar></MenuBar>
+      <div className="Content">
+        hello, world<br></br>
+        このページは現在工事中です
+      </div>
     </div>
-  </div>
   );
-}
+};
--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -3,11 +3,11 @@ import "./menubar.css";
 
 export const MenuBar: React.FC = () => {
   return (
-  <div className="MenuBar">
-    <div className="MenuBar__Logo">logo</div>
-    <input type="text" className="MenuBar__SearchInput"></input>
-    <button className="MenuBar__WriteButton">write</button>
-    <button className="MenuBar__AccountButton">account</button>
-  </div>
+    <div className="MenuBar">
+      <div className="MenuBar__Logo">logo</div>
+      <input type="text" className="MenuBar__SearchInput"></input>
+      <button className="MenuBar__WriteButton">write</button>
+      <button className="MenuBar__AccountButton">account</button>
+    </div>
   );
-}
+};
